f19ed764f1b79c37dfd97ba0f612ad6303bb1b24
Diego Marquina Rojas <mrdiego0892@gmail.com>
2019-03-14 01:55:22 -0500

    score design

--- lib/page/score.dart
+++ lib/page/score.dart
@@ -23,24 +23,46 @@ class ScorePageState extends State<ScorePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Puntaje')),
-      body: Container(
-        child: Center(
-          child: StreamBuilder(
-              stream: widget._model.getScoreBoard(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  Map<dynamic, dynamic> userInputs = Map.from(snapshot.data.value);
-                  return Column(
-                      children: userInputs.values
-                          .toList()
-                          .map((a) => Container(child: Text(a['username'])))
-                          .toList());
-                }else {
-                  return CircularProgressIndicator();
-                }
-              }),
-        ),
-      ),
+      body: StreamBuilder(
+          stream: widget._model.getScoreBoard(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              Map<dynamic, dynamic> userInputs = Map.from(snapshot.data.value);
+              return Column(
+                  children: userInputs.values
+                      .toList()
+                      .map((a) => Container(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
+                          child: Card(
+                              child: Container(
+                                  alignment: Alignment.center,
+                                  padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Text(a['score'].toString(),
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold, fontSize: 20.0)),
+                                      SizedBox(height: 2.0),
+                                      Text(a['username'],
+                                          style: TextStyle(fontWeight: FontWeight.bold)),
+                                      _buildInputs(a)
+                                    ],
+                                  )))))
+                      .toList());
+            } else {
+              return CircularProgressIndicator();
+            }
+          }),
     );
+  }
+
+  Widget _buildInputs(dynamic a) {
+    return Column(
+        children: Map.from(a['inputs'])
+            .map<dynamic, String>((key, value) => MapEntry(key, '$key: $value'))
+            .values
+            .toList()
+            .map((value) => Text(value, textAlign: TextAlign.left))
+            .toList());
   }
 }
